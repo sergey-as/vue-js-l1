76ea72cec3a3017c73072b7a99ac6f0f0062ecaf
sergey-as <sergey_as@ukr.net>
2021-08-03 21:06:17 +0300

    l1..

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--    <img alt="Vue logo" src="./assets/logo.png">-->
+    <!--    v-bind: => expression-->
+    <!--    <img v-bind:alt="imageAlt" v-bind:src="imageSrc">-->
+    <img :alt="imageAlt" :src="imageSrc">
+    <!--    <img :alt="2>3 ? imageAlt: 'kak'" :src="imageSrc+123">-->
+
+    <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
+    <div>{{ title }}</div>
+
+    <!--    <Counter />-->
+    <Counter :counter="counter"/>
+
+<!--    <button v-on:click="clickHandler">inc</button>-->
+    <button v-on:click="incHandler">inc</button>
+    <button v-on:click="decHandler">dec</button>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Counter from "./components/Counter";
+import Img from "./assets/logo.png"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //   HelloWorld,
+    //   Counter: Counter
+    Counter
+  },
+  data() {
+    return {
+      counter: 0,
+      title: 'Dynamic Text',
+      imageAlt: 'Vue logo',
+      imageSrc: Img,
+
+      // Тримати методи у data не правильно, тому що data для даних:
+      // clickHandler(){
+      //   console.log('kak')
+      // }
+    }
+  },
+  methods:{
+    incHandler(){
+      // console.log(this)
+      this.counter++;
+    },
+    decHandler(){
+      // console.log(this)
+      this.counter--;
+    }
+    38:00
+
   }
 }
 </script>
@@ -26,3 +70,12 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+<!--plan:-->
+<!--basic structure-->
+<!--embedding js into template-->
+<!--props-->
+<!--state-->
+<!--components-->
+<!--styling-->
+<!--lifecycle-->
---
+++ src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    counter value is {{ counter }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // counter: Number
+    counter: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
